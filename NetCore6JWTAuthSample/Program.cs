e56c85342e2c3d008c87cd376c27d472361638f4
AloKzMstf <alokazamustafa@gmail.com>
2022-08-17 12:13:13 +0300

    Gerekli impletmentasyonlar yapıldı.

--- NetCore6JWTAuthSample/Program.cs
+++ NetCore6JWTAuthSample/Program.cs
@@ -1,17 +1,46 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using NetCore6JWTAuthSample.DataContext;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
 
+//Entity Framework Core
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
                                                     options.UseSqlServer(configuration.GetConnectionString("ConStr")));
 
+//Identity Entity Framework Core
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDBContext>()
                                                           .AddDefaultTokenProviders();
+
+//Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    //JWT Bearer
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+
 
 builder.Services.AddControllers();
 
