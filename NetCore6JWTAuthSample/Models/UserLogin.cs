084c2b014657d419048ce7b93c54da2bd8b485c1
AloKzMstf <alokazamustafa@gmail.com>
2022-08-17 11:30:41 +0300

    Identity Models

---
+++ NetCore6JWTAuthSample/Models/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCore6JWTAuthSample.Models
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "User Name is required!")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        public string? Password { get; set; }
+    }
+}
---
+++ NetCore6JWTAuthSample/Models/UserRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCore6JWTAuthSample.Models
+{
+    public class UserRegistration
+    {
+        [Required(ErrorMessage = "User Name is invalid.")]
+        public string? Username { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is invalid.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is invalid.")]
+        public string? Password { get; set; }
+    }
+}
