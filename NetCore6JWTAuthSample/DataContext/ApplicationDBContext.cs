44f3511abe34586aca0b80cbae02d7da067b8441
AloKzMstf <alokazamustafa@gmail.com>
2022-08-17 11:17:15 +0300

    Identity DbContext

---
+++ NetCore6JWTAuthSample/DataContext/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetCore6JWTAuthSample.DataContext
+{
+    public class ApplicationDBContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
